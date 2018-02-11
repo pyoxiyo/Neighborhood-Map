0968922fce178e5b6082c6f5dae06dfe8b0e77be
pyoxiyo <pyoxiyo@gmail.com>
2018-02-11 04:34:18 +0300

    App is now responsive

--- scripts/w3.js
+++ scripts/w3.js
@@ -5,12 +5,16 @@ var hidden = true;
 hamburger.click(function() {
     if (hidden === true) {
         content.css("margin-left", "25%");
+        hamburger.addClass("hamburger-mobile");
+        $('.big-text-mobile').css("font-size", "1.9em");
         sidebar.css("display", "block");
         content.removeClass("m12");
         content.addClass("m9");
         hidden = false;
     } else if (hidden === false) {
         content.css("margin-left", "0");
+        hamburger.removeClass("hamburger-mobile");
+        $('.big-text-mobile').css("font-size", "2.1em");
         sidebar.css("display", "none");
         content.removeClass("m9");
         content.addClass("m12");
